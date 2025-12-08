1982ecda487592bcb2b80eefd571872daea5c886
RubenKristanto <rubenkristanto716@gmail.com>
2025-12-08 14:32:13 +0700

    danfo fix

--- vadapro_backend/controllers/fileController.js
+++ vadapro_backend/controllers/fileController.js
@@ -76,42 +76,15 @@ export const getCsvUrl = async (req, res) => {
 export const getCsvFromGridFS = async (req, res) => {
   try {
     const { entryId } = req.params;
-    // Use 'csv_uploads' bucket to match the upload function
-    const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'csv_uploads' });
+    const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'upload' });
     
-    // Find file by entryId in metadata OR by filename containing entryId (fallback)
-    // Note: The upload function doesn't currently set metadata.entryId. 
-    // We need to fix the upload or find a way to link them.
-    // For now, let's assume the file might be stored with entryId in metadata if uploaded correctly,
-    // or we might need to look up the process to get the fileId.
-    
-    // However, looking at the error "expected 1 fields but parsed 2", it implies it IS finding a file,
-    // but the content is wrong (maybe returning JSON error as CSV?).
-    
-    // Let's try to find the file more robustly.
-    // If we can't find by metadata, we might need to rely on the process model linking.
-    
-    // BUT, the previous code used bucketName: 'upload'. 
-    // If you have OLD files in 'upload' and NEW files in 'csv_uploads', we need to check both.
-    
-    let bucketToUse = bucket;
-    let files = await bucket.find({ 'metadata.entryId': entryId }).toArray();
-    
-    if (!files || files.length === 0) {
-       // Try the old bucket name 'upload' just in case
-       const oldBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'upload' });
-       files = await oldBucket.find({ 'metadata.entryId': entryId }).toArray();
-       if (files && files.length > 0) {
-           bucketToUse = oldBucket;
-       }
-    }
-
+    const files = await bucket.find({ 'metadata.entryId': entryId }).toArray();
     if (!files || files.length === 0) {
       return res.status(404).json({ success: false, message: 'CSV file not found' });
     }
     
     res.set('Content-Type', 'text/csv');
-    bucketToUse.openDownloadStream(files[0]._id).pipe(res);
+    bucket.openDownloadStream(files[0]._id).pipe(res);
   } catch (error) {
     console.error('GridFS error:', error);
     res.status(500).json({ success: false, message: error.message });
